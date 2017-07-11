7c4ec53f22ef9103f8290b999aca4761cbd8ecab
Howard Ting <cuberuru@gmail.com>
2017-07-11 04:38:09 +0000

    set up task page

--- scripts/cardclass.js
+++ scripts/cardclass.js
@@ -1,21 +1,4 @@
-Element.prototype.remove = function() {
-    this.parentElement.removeChild(this);
-}
-NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
-    for(var i = this.length - 1; i >= 0; i--) {
-        if(this[i] && this[i].parentElement) {
-            this[i].parentElement.removeChild(this[i]);
-        }
-    }
-}
 /* CARD MODULE */
-var driver = function() {
-    
-    var passengercoordinates = []; //push coordinates
-}
-var coordinates = function() {
-    
-}
 var card = function(fontsize, width, string, xpos, ypos) {
     this.fontsize = fontsize;
     this.height = fontsize/1.5;
@@ -23,7 +6,6 @@ var card = function(fontsize, width, string, xpos, ypos) {
     this.string = string;
     this.xpos = xpos;
     this.ypos = ypos;
-    this.focus = 0;
 }
 
 card.prototype.draw = function() {
@@ -32,17 +14,6 @@ card.prototype.draw = function() {
     cardbutton.textContent = this.string;
     document.body.appendChild(cardbutton);
 }
-
-card.prototype.drawat = function(xpos, ypos) {
-    this.xpos = xpos;
-    this.ypos = ypos;
-    var cardbutton = document.createElement("div");
-    cardbutton.setAttribute("style","position:absolute;top:"+this.ypos+"px;left:"+this.xpos+"px;padding-right:7px;padding-top:5px;padding-left:8px;border-radius: 5px;width:" + this.width + "px; height:" + this.height +"px; border:1px solid #000;");
-    cardbutton.className = 'item';
-    cardbutton.textContent = this.string;
-    document.body.appendChild(cardbutton);
-}
-
 var drawcards = function(cards) {
     for (var i = 0; i < cards.length; ++i) {
         cards[i].draw();
